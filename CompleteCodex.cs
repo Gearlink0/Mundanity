9a0cc232ec9151ddf69b7918e8c67c2a527cd057
Gearlink0 <gearlinkgmaster@aol.com>
2023-05-21 07:36:57 -0600

    Updated CompleteCodex to require charge and have a ZeroPointEnergyCollector

--- CompleteCodex.cs
+++ CompleteCodex.cs
@@ -6,12 +6,18 @@ using XRL.UI;
 
 namespace XRL.World.Parts
 {
-	public class MUNDANITY_CompleteCodex : IPart
+	public class MUNDANITY_CompleteCodex : IPoweredPart
 	{
 		public string Corpus = "LibraryCorpus.json";
 		public string Title = "complete codex";
 		[NonSerialized]
     public string Text;
+
+		public MUNDANITY_CompleteCodex()
+    {
+      this.ChargeUse = 10;
+		  this.WorksOnSelf = true;
+    }
 
 		public override bool WantEvent(int ID, int cascade)
     {
@@ -28,8 +34,33 @@ namespace XRL.World.Parts
     {
       if (E.Command == "Read" && E.Actor.IsPlayer())
       {
-        this.GeneratePage();
-        BookUI.ShowBook(this.Text, this.Title);
+				ActivePartStatus Status = this.GetActivePartStatus();
+				switch (Status)
+        {
+					case ActivePartStatus.Broken:
+		        Popup.ShowFail(this.ParentObject.Itis + " broken...");
+		        break;
+	        case ActivePartStatus.Booting:
+	          Popup.ShowFail(this.ParentObject.The + this.ParentObject.ShortDisplayName + this.ParentObject.Is + " still starting up.");
+						break;
+					case ActivePartStatus.Unpowered:
+						int Charge = this.ParentObject.QueryCharge();
+						if( Charge > 0 && Charge < this.ChargeUse )
+						{
+							Popup.ShowFail(this.ParentObject.The + this.ParentObject.ShortDisplayName + this.ParentObject.GetVerb("hum") + " for a moment, then powers down. " + this.ParentObject.It + this.ParentObject.GetVerb("don't") + " have enough charge to function.");
+	            break;
+	          }
+	          Popup.ShowFail(this.ParentObject.The + this.ParentObject.ShortDisplayName + this.ParentObject.GetVerb("don't") + " have enough charge to function.");
+						break;
+					case ActivePartStatus.Operational:
+						this.GeneratePage();
+						this.ConsumeCharge();
+		        BookUI.ShowBook(this.Text, this.Title);
+	  				break;
+					default:
+	          Popup.ShowFail("Nothing happens.");
+	          break;
+				}
       }
       return base.HandleEvent(E);
     }
