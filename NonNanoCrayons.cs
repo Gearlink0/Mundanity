6efef8b6540cb6997e970980d0e958fbbe62b8c2
Gearlink0 <gearlinkgmaster@aol.com>
2023-05-26 19:23:26 -0600

    Updated the PigmentBreeder to function like crayons without the chance of nanocrayons

---
+++ NonNanoCrayons.cs
@@ -0,0 +1,12 @@
+using XRL.Core;
+
+namespace XRL.World.Parts
+{
+  public class MUNDANITY_NonNanoCrayons : Crayons
+  {
+		public override bool HandleEvent(ObjectCreatedEvent E)
+    {
+      return base.HandleEvent(E);
+    }
+	}
+}
