d262b17aa8c7df40981e6a9433c43eaf7c768010
Gearlink0 <gearlinkgmaster@aol.com>
2025-01-12 14:04:47 -0700

    Updated sponger to actively soak up liquid in its cell, went back to using drams by evaporativity

--- Sponger.cs
+++ Sponger.cs
@@ -8,7 +8,7 @@ namespace XRL.World.Parts
   {
 		public int SPONGE_AMOUNT = 20;
     public int INITIAL_EVAPORATION = 5;
-    public int EVAPORATIVITY_FACTOR = 20;
+    public int EVAPORATIVITY_FACTOR = 5;
 
     public override bool WantEvent(int ID, int cascade)
     {
@@ -24,6 +24,7 @@ namespace XRL.World.Parts
     }
 
     public override bool HandleEvent(EndTurnEvent E) {
+      this.Sponge();
       LiquidCovered liquidCovering;
       if( this.ParentObject.TryGetEffect<LiquidCovered>(out liquidCovering) )
       {
@@ -36,10 +37,43 @@ namespace XRL.World.Parts
             if( EVAPORATIVITY_FACTOR.in100())
               ++dramsToEvaporate;
           }
-          liquidVolume.UseDrams( dramsToEvaporate );
+          liquidVolume.UseDramsByEvaporativity( dramsToEvaporate );
         }
       }
       return base.HandleEvent(E);
+    }
+
+    public bool Sponge()
+    {
+      Cell currentCell = this.ParentObject.GetCurrentCell();
+      if( currentCell != null )
+      {
+        LiquidVolume liquidVolume = currentCell.GetOpenLiquidVolume()?.LiquidVolume;
+        if( liquidVolume != null )
+        {
+          int existingVolume = 0;
+          int maxVolume = this.ParentObject.GetMaximumLiquidExposure();
+          LiquidCovered liquidCovering;
+          if( this.ParentObject.TryGetEffect<LiquidCovered>(out liquidCovering) )
+          {
+            if( liquidCovering.Liquid != null )
+              existingVolume = liquidCovering.Liquid.Volume;
+          }
+          if( existingVolume < maxVolume )
+          {
+            bool requestInterfaceExit = false;
+            liquidVolume.ProcessContact(
+              this.ParentObject,
+              Initial: true,
+              Poured: true,
+              PouredBy: this.ParentObject,
+              ContactVolume: (maxVolume - existingVolume)
+            );
+            return true;
+          }
+        }
+      }
+      return false;
     }
   }
 }
